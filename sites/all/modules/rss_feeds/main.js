01f88d3f6e55e20a94480172df475d0622027155
Riddya <riddya.work@gmail.com>
2013-10-28 18:02:47 +0300

    rss_feed add CSS & JS files

--- sites/all/modules/rss_feeds/main.js
+++ sites/all/modules/rss_feeds/main.js
@@ -1,3 +1,66 @@
-$(document).ready(function() {
+jQuery.fn.extend({
+    topMouseover: function(self) {
+        this.on('mouseover',function() {
+            self.opacity.stop().animate({opacity:"1"},300);
+            self.button.stop().animate({opacity:"1"},300);
+        });
+    },
+    topMouseout: function(self) {
+        this.on('mouseout',function() {
+            self.opacity.stop().animate({opacity:"0"},300);
+            self.button.stop().animate({opacity:"0.4"},300);
+        });
+    },
+    wScroll: function(g) {
+        if(g == 0) this.removeClass("no-count");
+        if(!this.hasClass("no-count")){
+            if(g > 500 && this.is(":hidden")){
+                this.addClass("visible");
+                this.css("cursor","pointer");
+                this.fadeIn(500);
+                this.click(function(){
+                    jQuery("body, html").animate({scrollTop:0},600);
+                    this.fadeOut(300);
+                    this.addClass("no-count");
+                });
+            }
+            if(g < 200 && this.hasClass("visible")){
+                this.removeClass("visible");
+                this.fadeOut(300);
+            }
+        }else{
+            this.unbind("click");
+        }
+    }
+});
 
+
+
+
+function toTopBtn(toTop, opacity, btn) {
+    this.toTopBtn = jQuery(toTop);
+    this.opacity = jQuery(opacity);
+    this.button = jQuery(btn);
+    this.windowScroll();
+    this._init();
+    return this;
+}
+
+toTopBtn.prototype = {
+    _init: function () {
+        var self = this;
+        this.toTopBtn.topMouseover(self);
+        this.toTopBtn.topMouseout(self);
+    },
+    windowScroll: function() {
+        var g = jQuery(window).scrollTop();
+        this.toTopBtn.wScroll(g);
+    }
+}
+$(document).ready(function() {
+    upButton = new toTopBtn(".toTopWrapper",".toTopOpacity",".toTopBtn");
+
+    jQuery(window).scroll(function(){
+        upButton.windowScroll();
+    });
 });
--- sites/all/modules/rss_feeds/rssfeeds-content.tpl.php
+++ sites/all/modules/rss_feeds/rssfeeds-content.tpl.php
@@ -1,3 +1,11 @@
+<div class="toTopWrapper">
+	<div class="toTopOpacity"></div>
+	<div class="toTopPanel" title="Up">
+		<div class="toTopBtn">
+			<span class="arrow">↑</span> <span class="label">up</span>
+		</div>
+	</div>
+</div>
 <br><br>
 <?php foreach ($items->channel->item as $item): ?>
     <span class = "title"><a href = "<?php echo $item->link;?>"><?php echo $item->title; ?></a></span><br><br>
